6f2eaad5dc06684dd8da887fc644072c76014128
Kate1234567891 <katehalle617@icloud.com>
2018-07-11 15:05:33 +0000

    Done.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Hi! 👋
+BrowserHi! 👋
 
 You've opened the IDE Sandbox. 🎉
 
---
+++ each.rb
@@ -0,0 +1,35 @@
+def four_letter_countries = [
+  "Chad",
+  "Cuba",
+  "Greenland",
+  "Iraq",
+  "Iran",
+  "Mali",
+  "Oman",
+  "Fiji"
+  ]
+  
+  def four_letter_countries = [
+    "Fiji",
+    "Oman",
+    "Mali",
+    "Iran",
+    "Iraq",
+    "Togo",
+    "Cuba",
+    "Chad",
+    "Laos"
+  ]
+  
+  def four_letter_countries = [
+    "Peru",
+    "Laos",
+    "Chad",
+    "Cuba",
+    "Togo",
+    "Iraq",
+    "Iran",
+    "Mali",
+    "Oman",
+    "Fiji"
+  ]
